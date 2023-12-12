429fcfb5b2af75fd0bee789f0e3e7d93b674f486
hyunsunglimm <gustjd3834@naver.com>
2023-12-12 19:37:22 +0900

    first commit

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo:
+    "https://velog.velcdn.com/images/gustjd3834/profile/4b318994-bf6a-4a9d-be2e-b31ea52ddfb2/image.png",
+  name: "ABGAUM / LimHyunSung",
+  email: "gustjd9610@gmail.com",
+  blog: "https://velog.io/@gustjd3834",
+  github: "https://github.com/hyunsunglimm",
+  repository: "https://github.com/ParkYoungWoong/vanillajs-movie-app",
+});
